b67f7a969d0670ebcde137abaf85aff653938818
hhsdev <heinhtetsoe.dev@gmail.com>
2019-10-01 18:24:28 +0630

    Add Prefix class

---
+++ huffman-compression/src/prefix.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include <string>
+class Prefix {
+ public:
+  Prefix() : code(0), codeLength(0) {}
+ 
+  void add(bool i) {
+    code <<= 1;
+    code |= i;
+	++codeLength;
+  }
+  int getCode() const { return code; }
+  int getCodeLength() const { return codeLength; }
+  std::string toString() { std::string ret;
+	for (int n = codeLength - 1; n >= 0; --n) {
+	  int nthBit = (code >> n) & 1;
+	  ret += (nthBit ? "1" : "0");
+	}
+	return ret;
+  }
+
+ private:
+  int code;
+  int codeLength;
+};
