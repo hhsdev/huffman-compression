b3cc76e9a0bffa44a6155b12f22deeaa24c08e67
hhsdev <heinhtetsoe.dev@gmail.com>
2019-11-04 00:31:18 +0630

    Implement more CodeWord class functions

--- huffman-compression/src/code_word.hpp
+++ huffman-compression/src/code_word.hpp
@@ -1,17 +1,38 @@
 #pragma once
 #include <string>
-class Prefix {
+#include <iostream>
+#include "util.hpp"
+
+class CodeWord {
  public:
-  Prefix() : code(0), codeLength(0) {}
-  Prefix(int code, int codeLength) : code(code), codeLength(codeLength) {}
-  void add(bool i) {
-    code <<= 1;
-    code |= i;
-    ++codeLength;
+  using code_t = unsigned int;
+  static constexpr int CODE_MAX = sizeof(code_t) * CHAR_BIT;
+  friend std::ostream& operator<<(std::ostream& os, const CodeWord& word);
+  static std::pair<CodeWord, CodeWord> split(const CodeWord& word, int firstCodeWordLength);
+
+  CodeWord() : code(0), codeLength(0) {}
+  CodeWord(code_t code, int codeLength) : CodeWord() {
+	this->code = code & ((1L<<codeLength)-1);
+	this->codeLength = codeLength;
+  }
+
+  void add(int code, int codeLength) {
+    this->code <<= codeLength;
+    this->code |= code;
+	this->codeLength += codeLength;
+  }
+  
+  bool willFit(const CodeWord& word) {
+	return word.length() + this->length() <= CODE_MAX;
+  }
+
+  int freeSpace() {
+	return CODE_MAX - codeLength;
   }
   int getCode() const { return code; }
-  int getCodeLength() const { return codeLength; }
-  std::string toString() {
+
+  int length() const { return codeLength; }
+  std::string toBitString() const {
     std::string ret;
     for (int n = codeLength - 1; n >= 0; --n) {
       int nthBit = (code >> n) & 1;
@@ -20,7 +41,24 @@ class Prefix {
     return ret;
   }
 
+  CodeWord operator+(const CodeWord& other) const {
+	CodeWord ret(*this);
+	ret.add(other.getCode(), other.length());
+	return ret;
+  }
+
+  CodeWord& operator+=(const CodeWord& other) {
+	this->add(other.getCode(), other.length());
+	return *this;
+  }
+
+  void clear() {
+	code = 0;
+	codeLength = 0;
+  }
  private:
   int code;
   int codeLength;
 };
+
+std::ostream& operator<<(std::ostream& os, const CodeWord& word);
