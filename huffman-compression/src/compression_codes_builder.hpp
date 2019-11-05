55fd3bd5c00e2824dac9a88f3924786737dc4af4
hhsdev <heinhtetsoe.dev@gmail.com>
2019-11-05 15:50:38 +0630

    Implement CompressionCodesBuilder class and tests

---
+++ huffman-compression/src/compression_codes_builder.hpp
@@ -0,0 +1,42 @@
+#ifndef H_CODE_WORD_ARRAY_BUILDER_H_
+#define H_CODE_WORD_ARRAY_BUILDER_H_
+
+#include "./code_word.hpp"
+#include "./util.hpp"
+
+template <typename FrequencyTree>
+class CompressionCodesBuilder {
+ public:
+  using CompressionCodes = Huffman::CharSizedArray<CodeWord>;
+  using node_t = typename FrequencyTree::node_t;
+
+  const CompressionCodes& buildFrom(const FrequencyTree& tree) {
+	if (tree.getRoot()->getLeft() != nullptr)
+	  accumulateCodeWords(tree.getRoot()->getLeft(), CodeWord(), true);
+	if (tree.getRoot()->getRight() != nullptr)
+	  accumulateCodeWords(tree.getRoot()->getRight(), CodeWord(), false);
+    return compressionCodes;
+  }
+
+  const CompressionCodes& getArray() const { return compressionCodes; }
+
+ private:
+  CompressionCodes compressionCodes;
+  void accumulateCodeWords(const node_t* node, const CodeWord& parent, bool isLeft) {
+    CodeWord current = parent;
+    current.add(isLeft ? 0 : 1, 1);
+	
+    if (node->hasChar()) { 
+	  compressionCodes[node->getChar()] = current;
+	  // every node holding a character is a leaf node
+	  return;
+	}
+
+    if (node->getLeft())
+      accumulateCodeWords(node->getLeft(), current, true);
+
+    if (node->getRight())
+      accumulateCodeWords(node->getRight(), current, false);
+  }
+};
+#endif
---
+++ test/src/compression_codes_builder.test.cpp
@@ -0,0 +1,78 @@
+#include "catch.hpp"
+#include "./compression_codes_builder.hpp"
+
+class MockNode {
+public:
+  MockNode(int frequency, char value) : frequency(frequency), value(value) {}
+
+  bool hasChar() const { return value >= 0; }
+  int getFrequency() const { return frequency; }
+  int getChar() const { return value; }
+  
+  void setFrequency(int frequency) { this->frequency = frequency; }
+  void setValue(int value) { this->value = value; }
+
+  MockNode* getLeft() { return left; }
+  MockNode* getRight() { return right; }
+
+  const MockNode* getLeft() const { return left; }
+  const MockNode* getRight() const { return right; }
+
+  void setLeft(MockNode* node) { left = node; }
+  void setRight(MockNode* node) { right = node; }
+  
+private:
+  int frequency; char value;
+  MockNode* left;
+  MockNode* right;
+};
+
+class MockTree {
+public:
+  using node_t = MockNode;
+  MockTree(MockNode* root) : root(root) {}
+  MockNode* getRoot() {
+	return root;
+  }
+  const MockNode* getRoot() const {
+	return root;
+  }
+private:
+  MockNode* root;
+};
+
+TEST_CASE("CompressionCodesBuilder", "[CompressionCodesBuilder]") {
+  // set up the tree in this way
+  /*
+   *          (root)
+   *          /    \
+   *        (a+b)  (c)
+   *        /   \
+   *      (a)   (b)
+   */
+  MockNode a(1, 'a'), b(2, 'b'), c(3, 'c');
+  MockNode aPlusB(a.getFrequency() + b.getFrequency(), -1);
+  MockNode root(aPlusB.getFrequency() + c.getFrequency(), -1);
+  aPlusB.setLeft(&a);
+  aPlusB.setRight(&b);
+  root.setLeft(&aPlusB);
+  root.setRight(&c);
+  MockTree tree(&root);
+  // test for mock objects
+  REQUIRE(a.getChar() == 'a');
+  REQUIRE(b.getChar() == 'b');
+  REQUIRE(c.getChar() == 'c');
+
+  REQUIRE(tree.getRoot() == &root);
+  REQUIRE(tree.getRoot()->getLeft() == &aPlusB);
+  REQUIRE(tree.getRoot()->getRight() == &c);
+  REQUIRE(tree.getRoot()->getLeft()->getLeft() == &a);
+  REQUIRE(tree.getRoot()->getLeft()->getRight() == &b);
+
+  CompressionCodesBuilder<MockTree> builder;
+  
+  auto codeWordArray = builder.buildFrom(tree);
+  REQUIRE(codeWordArray['a'].length() == 2);
+  REQUIRE(codeWordArray['b'].length() == 2);
+  REQUIRE(codeWordArray['c'].length() == 1);
+}
