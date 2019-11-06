4acca5d3f828b6e8a4b37d976483cdcf6092ca59
hhsdev <heinhtetsoe.dev@gmail.com>
2019-11-06 15:45:13 +0630

    Clean up CompressionCodesBuilder class

--- huffman-compression/src/compression_codes_builder.hpp
+++ huffman-compression/src/compression_codes_builder.hpp
@@ -10,33 +10,41 @@ class CompressionCodesBuilder {
   using CompressionCodes = Huffman::CharSizedArray<CodeWord>;
   using node_t = typename FrequencyTree::node_t;
 
-  const CompressionCodes& buildFrom(const FrequencyTree& tree) {
-	if (tree.getRoot()->getLeft() != nullptr)
-	  accumulateCodeWords(tree.getRoot()->getLeft(), CodeWord(), true);
-	if (tree.getRoot()->getRight() != nullptr)
-	  accumulateCodeWords(tree.getRoot()->getRight(), CodeWord(), false);
-    return compressionCodes;
-  }
-
+  const CompressionCodes& buildFrom(const FrequencyTree& tree);
   const CompressionCodes& getArray() const { return compressionCodes; }
 
  private:
   CompressionCodes compressionCodes;
-  void accumulateCodeWords(const node_t* node, const CodeWord& parent, bool isLeft) {
-    CodeWord current = parent;
-    current.add(isLeft ? 0 : 1, 1);
-	
-    if (node->hasChar()) { 
-	  compressionCodes[node->getChar()] = current;
-	  // every node holding a character is a leaf node
-	  return;
-	}
-
-    if (node->getLeft())
-      accumulateCodeWords(node->getLeft(), current, true);
-
-    if (node->getRight())
-      accumulateCodeWords(node->getRight(), current, false);
-  }
+  void accumulateCodeWords(const node_t* node, const CodeWord& parent,
+                           bool isLeft);
 };
+
+
+const CompressionCodes& CompressionCodesBuilder::buildFrom(
+    const FrequencyTree& tree) {
+  if (tree.getRoot()->getLeft() != nullptr)
+    accumulateCodeWords(tree.getRoot()->getLeft(), CodeWord(), true);
+
+  if (tree.getRoot()->getRight() != nullptr)
+    accumulateCodeWords(tree.getRoot()->getRight(), CodeWord(), false);
+
+  return compressionCodes;
+}
+
+void CompressionCodesBuilder::accumulateCodeWords(const node_t* node,
+                                                  const CodeWord& parent,
+                                                  bool isLeft) {
+  CodeWord current = parent;
+  current.add(isLeft ? 0 : 1, 1);
+
+  if (node->hasChar()) {
+    compressionCodes[node->getChar()] = current;
+    // every node holding a character is a leaf node
+    // so no need to go further
+    return;
+  }
+
+  if (node->getLeft()) accumulateCodeWords(node->getLeft(), current, true);
+  if (node->getRight()) accumulateCodeWords(node->getRight(), current, false);
+}
 #endif
