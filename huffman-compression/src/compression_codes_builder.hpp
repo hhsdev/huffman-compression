f86d838cf8e7f526b54017b30ab4c725b294d0d9
hhsdev <heinhtetsoe.dev@gmail.com>
2019-11-16 02:50:41 +0630

    Minor refactorings

--- huffman-compression/src/compression_codes_builder.hpp
+++ huffman-compression/src/compression_codes_builder.hpp
@@ -2,7 +2,7 @@
 #define H_CODE_WORD_ARRAY_BUILDER_H_
 #include <memory>
 
-#include "./base_bitset.hpp"
+#include "./all_bitsets.hpp"
 #include "./bitset_32.hpp"
 #include "./dynamic_bitset.hpp"
 #include "./util.hpp"
@@ -18,19 +18,23 @@ class CompressionCodesBuilder {
 
  private:
   CompressionCodes compressionCodes;
-  BaseBitset* chooseAppropriateBitset(const FrequencyTree& tree) const;
   void accumulateCodeWords(const node_t* node, BaseBitset* parent, bool isLeft);
 };
 
 template <typename FrequencyTree>
 const typename CompressionCodesBuilder<FrequencyTree>::CompressionCodes&
 CompressionCodesBuilder<FrequencyTree>::buildFrom(const FrequencyTree& tree) {
-  std::unique_ptr<BaseBitset> baseBitset(chooseAppropriateBitset(tree));
+
+  auto genesisBitset = getLargeEnoughBitset(tree.getHeight());
+
   if (tree.getRoot()->getLeft() != nullptr)
-    accumulateCodeWords(tree.getRoot()->getLeft(), baseBitset->clone(), true);
+    accumulateCodeWords(tree.getRoot()->getLeft(), genesisBitset->clone(),
+                        true);
 
   if (tree.getRoot()->getRight() != nullptr)
-    accumulateCodeWords(tree.getRoot()->getRight(), baseBitset->clone(), false);
+    accumulateCodeWords(tree.getRoot()->getRight(), genesisBitset->clone(),
+                        false);
+
   return compressionCodes;
 }
 
@@ -52,12 +56,4 @@ void CompressionCodesBuilder<FrequencyTree>::accumulateCodeWords(
   delete current;
 }
 
-template <typename FrequencyTree>
-BaseBitset* CompressionCodesBuilder<FrequencyTree>::chooseAppropriateBitset(
-    const FrequencyTree& tree) const {
-  if (tree.getHeight() <= 32)
-	return new Bitset32();
-  else
-	return new DynamicBitset();
-}
 #endif
