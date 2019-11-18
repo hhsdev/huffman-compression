9589f0c9cb578e81e67df55536e05146ecfe7d68
hhsdev <heinhtetsoe.dev@gmail.com>
2019-11-19 05:24:32 +0630

    Optimize DynamicBitset class
    * DynamicBitset class is changed into a template taking a type to use
      as blocks.
    * push_back function is furthur optimized by having an extra block to
      use as buffer. Only when the buffer is full is the block pushed into
      the block container.

--- huffman-compression/src/dynamic_bitset.hpp
+++ huffman-compression/src/dynamic_bitset.hpp
@@ -1,34 +1,163 @@
 #pragma once
 
 #include <deque>
+#include <vector>
+#include <iostream>
 #include <climits>
 #include "./base_bitset.hpp"
 
+namespace IndexOperations {
+
+template <typename Block>
+int bitToBlock(int bitIndex) {
+  return bitIndex / (sizeof(Block) * CHAR_BIT);
+}
+
+template <typename Block>
+int byteToBlock(int byteIndex) {
+  return byteIndex / sizeof(Block);
+}
+
+template <typename Block>
+int bitOffset(int bitIndex) {
+  return bitIndex % (sizeof(Block) * CHAR_BIT);
+}
+
+template <typename Block>
+int byteOffset(int byteIndex) {
+  return byteIndex % sizeof(Block);
+}
+
+}  // namespace IndexOperations
+
+template <typename Block = uint64_t>
 class DynamicBitset : public BaseBitset {
  public:
-  DynamicBitset();
-
-  void push_back(bool val) override;
+  using block_type = Block; 
+  DynamicBitset() : BaseBitset(), blocks(), bufferBlock(0), bufferBlockSize(0) {}
   
-  bool operator[](int index) const;
-  reference operator[](int index);
-  BaseBitset& operator++() override;
+  void push_back(bool val) override {
+	const int sz = size();
+	if (bufferBlockSize == sizeof(bufferBlock) * CHAR_BIT) {
+	  blocks.push_back(bufferBlock);
+	  bufferBlock = val;
+	  bufferBlockSize = 1;
+	}
+	else {
+	  bufferBlock = (bufferBlock << 1) | val;
+	  ++bufferBlockSize;
+	}
+  }
 
-  void clear() override;
-  unsigned char getByte(int index) const override;
+  bool operator[](int bitIndex) const override {
+	return get(bitIndex);
+  }
 
-  int size() const override;
+  reference operator[](int index) {
+	return reference(this, index);
+  }
 
-  std::string toString() const override;
+  BaseBitset& operator++() override {
+	++bufferBlock;
+	if (!isPowerOf2(bufferBlock)) return *this;
+	Block mask = ~Block(0) >> (sizeof(Block) * CHAR_BIT - bufferBlockSize);
+	bufferBlock &= mask;
+	const int numBlocks = blocks.size();
+	for (int i = 0; i < numBlocks; ++i) {
+	  ++blocks[i];
+	  // we will break out of the loop unless the incremented block overflows
+	  if (blocks[i] != 0) {
+		break;
+	  }
+	}
+	return *this;
+  }
 
-  BaseBitset* clone() const override;
+
+  void clear() override {
+	blocks.clear();
+	bufferBlock = 0;
+	bufferBlockSize = 0;
+  }
+
+  unsigned char getByte(int byteIndex) const override {
+	unsigned char ret = 0;
+	const int bitIndex = byteIndex * CHAR_BIT;
+	for (int i = std::min(bitIndex + CHAR_BIT - 1, size() - 1); i >= bitIndex; --i) {
+	  ret = (ret << 1) | get(i);
+	}
+	return ret;
+  }
+
+  int size() const override { return bufferBlockSize + (blocks.size() * sizeof(Block) * CHAR_BIT); }
+
+  std::string toString() const override {
+	std::string ret;
+	const int sz = size();
+	for (int i = sz - 1; i >= 0; --i) {
+	  ret.push_back(get(i) ? '1' : '0');
+	}
+	return ret;
+  }
+
+  BaseBitset* clone() const override { return new DynamicBitset(*this); }
 
  protected:
-  bool get(int index) const override;
-  void set(int index) override;
-  void unset(int index) override;
+  bool get(int bitIndex) const override {
+	if (bitIndex < bufferBlockSize) {
+	  return nthBit(bufferBlock, bitIndex);
+	} else {
+	  int adjustedBitIndex = bitIndex - bufferBlockSize;
+	  int blockIndex = blocks.size() - 1 - (adjustedBitIndex / (sizeof(Block) * CHAR_BIT));
+	  int bitOffset = adjustedBitIndex % (sizeof(Block) * CHAR_BIT);
+	  return nthBit(blocks[blockIndex], bitOffset);
+	}
+  }
 
+  bool nthBit(Block block, int index) const {
+	return (block >> index) & 1;
+  }
+
+  unsigned char nthByte(Block block, int index) const {
+	return (block >> (index * CHAR_BIT)) & 0b1111'1111;
+  }
+
+  void set(int bitIndex) override {
+	if (bitIndex < bufferBlockSize) {
+	  const int bitOffset = IndexOperations::bitOffset<Block>(bitIndex);
+	  const Block mask = Block(1) << bitOffset;
+	  bufferBlock |= mask;
+	} else {
+	  bitIndex -= bufferBlockSize;
+	  auto blockIndex = IndexOperations::bitToBlock<Block>(bitIndex);
+	  auto bitOffset = IndexOperations::bitOffset<Block>(bitIndex);
+	  Block mask = Block(1) << bitOffset;	
+	  blocks[blockIndex] |= mask;
+	}
+  }
+
+  void unset(int bitIndex) override {
+	if (bitIndex < bufferBlockSize) {
+	  auto bitOffset = IndexOperations::bitOffset<Block>(bitIndex);
+	  Block mask = ~(Block(1) << bitOffset);
+	  bufferBlock &= mask;
+	} else {
+	  bitIndex -= bufferBlockSize;
+	  auto blockIndex = IndexOperations::bitToBlock<Block>(bitIndex);
+	  auto bitOffset = IndexOperations::bitOffset<Block>(bitIndex);
+	  Block mask = ~(Block(1) << bitOffset);
+	  blocks[blockIndex] &= mask;
+	}
+  }
  private:
-  std::deque<unsigned char> container;
-  int bitSize;
+  bool isPowerOf2(Block n) {
+	return (n & (n - 1)) == 0;
+  }
+  void pushBackNewBlock(block_type value) {
+	blocks.push_back(value);
+  }
+  std::vector<Block> blocks;
+  Block bufferBlock;
+  int bufferBlockSize;
 };
+
--- test/src/dynamic_bitset.test.cpp
+++ test/src/dynamic_bitset.test.cpp
@@ -1,25 +1,30 @@
-#include "catch.hpp"
+#include "./bitset_common.test.hpp"
 #include "./dynamic_bitset.hpp"
+#include "catch.hpp"
 
-TEST_CASE("dynamic_bitset", "[DynamicBitset]") {
-  DynamicBitset db;
-  db.push_back(0);
-  db.push_back(1);
-  REQUIRE(db.size() == 2);
-  REQUIRE(db.toString() == "01");
+TEST_CASE("push_back works as intended", "[DynamicBitset]") {
+  const std::vector<std::string> testStrings = {
+      "0000100000111101111010000101011011001111000010111010110001011111010010"
+      "1101001100100101101101001100111100100010100101011101011001100111101100"
+      "101101101011010000011100101011110110111111101000111111",
 
-  db.push_back(0);
-  db.push_back(1);
-  db.push_back(1);
-  REQUIRE(db.toString() == "01011");
-  db[2] = 1;
-  db[4] = 1;
-  REQUIRE(db.toString() == "11111");
-  REQUIRE(db.size() == 5);
+      "0000000011111111000000001111111110000000111111110000000011111111"
+      "0000000011111111000000001111111110000000111111110000000011111111"
+      "0000000011111111000000001111111110000000111111110000000011111111"
+      "0000000011111111000000001111111110000000111111110000000011111111",
 
-  REQUIRE(db.getByte(0) == 0b0001'1111);
-  auto newDb = db.clone();
-  REQUIRE(newDb->toString() == "11111");
-
-  delete newDb;
+      "10000000000000000000000000000000"
+      "11000000000000000000000000000000"
+      "11100000000000000000000000000000"
+      "11110000000000000000000000000000"
+      "11111000000000000000000000000000"
+      "11111100000000000000000000000000"
+      "11111110000000000000000000000000"
+      "11111111000000000000000000000000"};
+  
+  for (const std::string& testString : testStrings) {
+	DynamicBitset<> bits;
+	repeatedlyCallPushBack(bits, testString);
+  	assertDataAndSizeEquals(bits, testString);
+  }
 }
