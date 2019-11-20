82f02470e09794d066928a87ccde3740970129b7
hhsdev <heinhtetsoe.dev@gmail.com>
2019-11-20 07:10:34 +0630

    Make BitUtil functions inline

--- huffman-compression/src/bit_util.cpp
+++ huffman-compression/src/bit_util.cpp
@@ -1,26 +1,2 @@
 #include <cmath>
 #include "./bit_util.hpp"
-namespace BitUtil {
-
-std::pair<unsigned char, unsigned char> split(unsigned char byte,
-                                              int frontBitsLength) {
-  const int backBitsLength = CHAR_BIT - frontBitsLength;
-  const unsigned char frontPart = byte >> backBitsLength;
-  // this makes a mask with value 0b111..backBitsLength times..11
-  const unsigned char mask = (1 << backBitsLength) - 1;
-  const unsigned char backPart = byte & mask;
-  
-  return {frontPart, backPart};
-}
-
-int bitOffset(int bitIndex) {
-  return bitIndex % CHAR_BIT;
-}
-
-int toByteIndex(int bitIndex) {
-  return bitIndex / CHAR_BIT;
-}
-int toByteSize(int bitSize) {
-  return std::ceil(bitSize / (double)CHAR_BIT);
-}
-}
