08c1cff80a251d53a9819fdc2be3c98631b4e535
hhsdev <heinhtetsoe.dev@gmail.com>
2020-04-03 10:58:50 +0630

    Format decompressor.hpp

--- huffman-compression/src/decompressor.hpp
+++ huffman-compression/src/decompressor.hpp
@@ -1,29 +1,30 @@
 #pragma once
 #include <exception>
-#include "./huffman_tree.hpp"
 #include "./base_bitset.hpp"
+#include "./huffman_tree.hpp"
 class HuffmanTree;
 
 class Decompressor {
  public:
-  std::string decompress(const HuffmanTree& tree, const BaseBitset& bits) const {
-	std::string ret;
-	const HuffmanNode* curr = tree.getRoot();
-	if (!curr) {
-	  throw std::runtime_error("Tree is empty");
-	}
-	for (int i = bits.size() - 1; i >= 0; --i) {
-	  bool currentBit = bits[i];
-	  if (currentBit) {
-		curr = curr->getRight();
-	  } else {
-		curr = curr->getLeft();
-	  }
-	  if (!curr->getLeft() && !curr->getRight()) {
-		ret.push_back(curr->getChar());
-		curr = tree.getRoot();
-	  }
-	}
-	return ret;
+  std::string decompress(const HuffmanTree& tree,
+                         const BaseBitset& bits) const {
+    std::string ret;
+    const HuffmanNode* curr = tree.getRoot();
+    if (!curr) {
+      throw std::runtime_error("Tree is empty");
+    }
+    for (int i = bits.size() - 1; i >= 0; --i) {
+      bool currentBit = bits[i];
+      if (currentBit) {
+        curr = curr->getRight();
+      } else {
+        curr = curr->getLeft();
+      }
+      if (!curr->getLeft() && !curr->getRight()) {
+        ret.push_back(curr->getChar());
+        curr = tree.getRoot();
+      }
+    }
+    return ret;
   }
 };
