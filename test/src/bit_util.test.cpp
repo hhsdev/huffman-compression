802b0646dfe982fc29e1300adff25c92ac2ba117
hhsdev <heinhtetsoe.dev@gmail.com>
2020-09-03 20:04:09 +0630

    Fix formatting

--- test/src/bit_util.test.cpp
+++ test/src/bit_util.test.cpp
@@ -7,7 +7,7 @@ TEST_CASE("Bit twiddling utilities", "[BitUtil]") {
   REQUIRE(front == 0b1111);
   REQUIRE(back == 0b1010);
 
-  REQUIRE(nthBit(0b1011, 2) == 0); 
+  REQUIRE(nthBit(0b1011, 2) == 0);
   REQUIRE(nthBit(0b1011, 1) == 1); 
 }
 
